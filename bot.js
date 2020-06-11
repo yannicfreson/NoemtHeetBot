be4c2bd57aa31ec5e1a0a03d241591a5c837f565
Storm <yannic.freson1@gmail.com>
2020-06-11 02:33:53 +0200

    Mistakes for everyone!
    Added "!andersFout" command so the bot can correct mistakes made by other people without changing the counter.

--- bot.js
+++ bot.js
@@ -9,88 +9,107 @@ let whichOne = 0;
 var dispatcher = null;
 
 try {
-    mistakes = JSON.parse(fs.readFileSync('./storage.json',"utf8")).mistakes;
+    mistakes = JSON.parse(fs.readFileSync('./storage.json', "utf8")).mistakes;
     console.log('Reeds ' + mistakes + ' fouten gemaakt');
-        
+
 } catch (error) {
-    console.log("mistakes.txt bestaat nog niet",error)
+    console.log("mistakes.txt bestaat nog niet", error)
 }
 
-function setStatus () {
+function setStatus() {
     client.user.setActivity(mistakes.toString(), { type: 'LISTENING' });
 }
 
 const client = new Discord.Client();
- 
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
     setStatus();
 });
 
 client.on('message', async msg => {
-  if (msg.content === '!ping') {
-    msg.channel.send('!pong');
-  }
 
-  if (msg.content.substring(0, 1) === '!') {
-    let args = msg.content.substring(1).split(' ');
-    const cmd = args[0];
-   
+    if (msg.content.substring(0, 1) === '!') {
+        let args = msg.content.substring(1).split(' ');
+        const cmd = toString(args[0]).toLowerCase();
 
-    
-    args = args.splice(1);
-    switch(cmd) {
-        case 'fout':
-            try {
-                connection = await msg.member.voice.channel.join();
-                currentChannel = msg.member.voice.channel;
-                console.log('Now playing!');
+        args = args.splice(1);
+        switch (cmd) {
+            case 'fout':
+                try {
+                    connection = await msg.member.voice.channel.join();
+                    currentChannel = msg.member.voice.channel;
+                    console.log('Now playing!');
 
-                if (whichOne == 0) {
-                    dispatcher = connection.play('./sounds/heet.mp3');
-                    whichOne = 1;
-                } else if (whichOne == 1) {
-                    dispatcher = connection.play('./sounds/heet1.mp3');
-                    whichOne = 0
+                    if (whichOne == 0) {
+                        dispatcher = connection.play('./sounds/heet.mp3');
+                        whichOne = 1;
+                    } else if (whichOne == 1) {
+                        dispatcher = connection.play('./sounds/heet1.mp3');
+                        whichOne = 0
+                    }
+
+                    dispatcher.setVolume(0.7);
+                    dispatcher.on('finish', () => {
+                        currentChannel.leave();
+                        console.log('Finished playing!\nLeft channel.');
+                    });
+                } catch (error) {
+                    console.log('error playing the sound');
                 }
+                mistakes++
+                msg.channel.send('Fout toegevoegd. Aantal fouten: ' + mistakes)
+                break;
 
-                dispatcher.setVolume(0.7);
-                dispatcher.on('finish', () => {
-                    currentChannel.leave();
-                    console.log('Finished playing!\nLeft channel.');
-                });
-            } catch (error) {
-                console.log('error playing the sound');
-            }
-            mistakes++
-            msg.channel.send('Fout toegevoegd. Aantal fouten: ' + mistakes)
-            break;
+            case 'anderfout':
+                try {
+                    connection = await msg.member.voice.channel.join();
+                    currentChannel = msg.member.voice.channel;
+                    console.log('Now playing!');
 
-        case '-fout':
-            if (mistakes > 0) {
-                mistakes--
-                msg.channel.send('Fout verwijderd. Aantal fouten: ' + mistakes)
-            } else {
-                msg.channel.send('Huidig aantal fouten is 0.')
-            }
-            break;
+                    if (whichOne == 0) {
+                        dispatcher = connection.play('./sounds/heet.mp3');
+                        whichOne = 1;
+                    } else if (whichOne == 1) {
+                        dispatcher = connection.play('./sounds/heet1.mp3');
+                        whichOne = 0
+                    }
 
-        case 'resetCounter':
-            if (msg.author.tag == 'Storm#1131') {
-                mistakes = 0
-                msg.channel.send('Counter reset. Huidig aantal fouten: ' + mistakes)
-            }
-            break;
+                    dispatcher.setVolume(0.7);
+                    dispatcher.on('finish', () => {
+                        currentChannel.leave();
+                        console.log('Finished playing!\nLeft channel.');
+                    });
+                } catch (error) {
+                    console.log('error playing the sound');
+                }
+                break;
 
-        case 'aantal':
-            msg.channel.send('Huidig aantal fouten: ' + mistakes)
-            break;
-     }
-     fs.writeFileSync('storage.json', JSON.stringify({
-         mistakes
-     }))
-     setStatus();
- }
+            case '-fout':
+                if (mistakes > 0) {
+                    mistakes--
+                    msg.channel.send('Fout verwijderd. Aantal fouten: ' + mistakes)
+                } else {
+                    msg.channel.send('Huidig aantal fouten is 0.')
+                }
+                break;
+
+            case 'resetCounter':
+                if (msg.author.tag == 'Storm#1131') {
+                    mistakes = 0
+                    msg.channel.send('Counter reset. Huidig aantal fouten: ' + mistakes)
+                }
+                break;
+
+            case 'aantal':
+                msg.channel.send('Huidig aantal fouten: ' + mistakes)
+                break;
+        }
+        fs.writeFileSync('storage.json', JSON.stringify({
+            mistakes
+        }))
+        setStatus();
+    }
 });
 
 client.login(auth.token);
